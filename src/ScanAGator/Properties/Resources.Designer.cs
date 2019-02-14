d902b718a53a060d859a7e12e459bf505f9cb90c
Scott W Harden <swharden@gmail.com>
2019-02-13 19:24:12 -0500

    version 1.5 started

--- src/ScanAGator/Properties/Resources.Designer.cs
+++ src/ScanAGator/Properties/Resources.Designer.cs
@@ -61,7 +61,7 @@ namespace ScanAGator.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to 1.4.
+        ///   Looks up a localized string similar to 1.5.
         /// </summary>
         internal static string version {
             get {
