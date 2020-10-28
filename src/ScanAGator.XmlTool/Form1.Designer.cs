d05630aaeb1272582c26236c98d3c5c33927707d
Scott W Harden <swharden@gmail.com>
2020-10-28 15:01:54 -0400

    dont copy headers to clipboard

--- src/ScanAGator.XmlTool/Form1.Designer.cs
+++ src/ScanAGator.XmlTool/Form1.Designer.cs
@@ -61,6 +61,7 @@
             this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 64);
             this.dataGridView1.Name = "dataGridView1";
