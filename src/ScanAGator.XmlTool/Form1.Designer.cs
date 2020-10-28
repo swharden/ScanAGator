32c30d9fe3c147678bc257bf513b40d196f55d3a
Scott W Harden <swharden@gmail.com>
2020-10-28 15:02:34 -0400

    make cells readonly

--- src/ScanAGator.XmlTool/Form1.Designer.cs
+++ src/ScanAGator.XmlTool/Form1.Designer.cs
@@ -65,6 +65,7 @@
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(12, 64);
             this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
             this.dataGridView1.Size = new System.Drawing.Size(776, 374);
             this.dataGridView1.TabIndex = 2;
             // 
