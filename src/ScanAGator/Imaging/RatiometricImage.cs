ede599843b02418bb76341db0fe83e965def2191
Scott W Harden <swharden@gmail.com>
2022-08-23 11:47:17 -0400

    RatiometricImage: calculate bitmaps on first request

--- src/ScanAGator/Imaging/RatiometricImage.cs
+++ src/ScanAGator/Imaging/RatiometricImage.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace ScanAGator.Imaging;
 
@@ -7,27 +8,55 @@ public class RatiometricImage
     public readonly ImageData GreenData;
     public readonly ImageData RedData;
 
-    public readonly Bitmap Green;
-    public readonly Bitmap Red;
-    public readonly Bitmap Merge;
+    private Bitmap? _Green = null;
+    private Bitmap? _Red = null;
+    private Bitmap? _Merge = null;
+
+    public Bitmap Green
+    {
+        get
+        {
+            if (_Green is null)
+            {
+                _Green = ImageDataTools.GetBitmapIndexed(GreenData);
+            }
+            return _Green;
+        }
+    }
+
+    public Bitmap Red
+    {
+        get
+        {
+            if (_Red is null)
+            {
+                _Red = ImageDataTools.GetBitmapIndexed(RedData);
+            }
+            return _Red;
+        }
+    }
+
+    public Bitmap Merge
+    {
+        get
+        {
+            if (_Merge is null)
+            {
+                _Merge = ImageDataTools.Merge(RedData, GreenData, RedData);
+            }
+            return _Merge;
+        }
+    }
 
     public RatiometricImage(ImageData green, ImageData red)
     {
         GreenData = green;
         RedData = red;
-
-        Green = ImageDataTools.GetBitmapIndexed(GreenData);
-        Red = ImageDataTools.GetBitmapIndexed(RedData);
-        Merge = ImageDataTools.Merge(RedData, GreenData, RedData);
     }
 
     public RatiometricImage(string greenFilePath, string redFilePath)
     {
         GreenData = ImageDataTools.ReadTif(greenFilePath);
         RedData = ImageDataTools.ReadTif(redFilePath);
-
-        Green = ImageDataTools.GetBitmapIndexed(GreenData);
-        Red = ImageDataTools.GetBitmapIndexed(RedData);
-        Merge = ImageDataTools.Merge(RedData, GreenData, RedData);
     }
 }
