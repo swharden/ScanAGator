a0dbca2ac16bcbc7d46381d13677c91333e6d252
Scott W Harden <swharden@gmail.com>
2022-08-23 15:39:08 -0400

    update deployment script

--- dev/deploy.bat
+++ dev/deploy.bat
@@ -1,4 +1,5 @@
 rmdir /s /q X:\Software\Scan-A-Gator
+rmdir /s /q ..\src\ScanAGator\bin
 dotnet publish --configuration Release ..\src
 robocopy ..\src\ScanAGator\bin\Release\net48\publish X:\Software\Scan-A-Gator /E /NJH /NFL /NDL
 pause
