945d3ec377fec55362b4cd9ca9273ce64fa29ab4
Scott W Harden <swharden@gmail.com>
2022-08-23 12:43:14 -0400

    improve graphs

--- src/ScanAGator/GUI/AnalysisResultsControl.cs
+++ src/ScanAGator/GUI/AnalysisResultsControl.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using ScottPlot;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ScanAGator.GUI
@@ -26,29 +21,40 @@ namespace ScanAGator.GUI
 
         public void ShowResult(Analysis.AnalysisResult result)
         {
-            double samplePeriod = result.Settings.Xml.MsecPerPixel;
-            double sampleRate = 1.0 / samplePeriod;
-            double[] xs = ScottPlot.DataGen.Consecutive(result.GreenCurve.Values.Length, samplePeriod);
+            double[] xs = GetTimesMsec(result);
 
             formsPlot1.Plot.Clear();
             formsPlot1.Plot.AddScatterPoints(xs, result.GreenCurve.Values, Color.FromArgb(30, Color.Green));
             formsPlot1.Plot.AddScatterPoints(xs, result.RedCurve.Values, Color.FromArgb(30, Color.Red));
-            formsPlot1.Plot.AddSignal(result.SmoothGreenCurve.Values, sampleRate, Color.Green);
-            formsPlot1.Plot.AddSignal(result.SmoothRedCurve.Values, sampleRate, Color.Red);
+            formsPlot1.Plot.AddScatterLines(xs, result.SmoothGreenCurve.Values, Color.Green);
+            formsPlot1.Plot.AddScatterLines(xs, result.SmoothRedCurve.Values, Color.Red);
+            ShadeBaseline(formsPlot1.Plot, result);
+            formsPlot1.Plot.SetAxisLimits(yMin: 0);
             formsPlot1.Plot.YLabel("Fluorescence (AFU)");
             formsPlot1.Refresh();
 
             formsPlot2.Plot.Clear();
-            formsPlot2.Plot.AddSignal(result.SmoothDeltaGreenOverRedCurve.Values, sampleRate);
-            formsPlot2.Plot.AddHorizontalSpan(
-                xMin: result.Settings.Baseline.Min * samplePeriod, 
-                xMax: result.Settings.Baseline.Max * samplePeriod, 
-                color: Color.FromArgb(20, Color.Black));
+            formsPlot2.Plot.AddScatterLines(xs, result.SmoothDeltaGreenOverRedCurve.Values);
+            ShadeBaseline(formsPlot2.Plot, result);
             formsPlot2.Plot.AddHorizontalLine(0, Color.Black, 0, ScottPlot.LineStyle.Dash);
             formsPlot2.Plot.YLabel("ΔF/F (%)");
+            formsPlot2.Plot.MatchLayout(formsPlot1.Plot);
             formsPlot2.Refresh();
 
             dataExportControl1.ShowResult(result);
+        }
+
+        private static void ShadeBaseline(Plot plt, Analysis.AnalysisResult result)
+        {
+            plt.AddHorizontalSpan(
+                xMin: result.Settings.Baseline.Min * result.Settings.Xml.MsecPerPixel,
+                xMax: result.Settings.Baseline.Max * result.Settings.Xml.MsecPerPixel,
+                color: Color.FromArgb(20, Color.Black));
+        }
+
+        private double[] GetTimesMsec(Analysis.AnalysisResult result)
+        {
+            return DataGen.Consecutive(result.GreenCurve.Values.Length, result.Settings.Xml.MsecPerPixel);
         }
     }
 }
