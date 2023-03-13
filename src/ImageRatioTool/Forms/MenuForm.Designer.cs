83cad348ff1a0cb1057f8d9c82b3c86fb8888fb8
Scott W Harden <swharden@gmail.com>
2023-03-13 16:05:47 -0400

    XML file inspector

--- src/ImageRatioTool/Forms/MenuForm.Designer.cs
+++ src/ImageRatioTool/Forms/MenuForm.Designer.cs
@@ -28,75 +28,80 @@ partial class MenuForm
     /// </summary>
     private void InitializeComponent()
     {
-            this.btnSquareRoi = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.btnDendriteRois = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // btnSquareRoi
-            // 
-            this.btnSquareRoi.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnSquareRoi.Location = new System.Drawing.Point(26, 126);
-            this.btnSquareRoi.Name = "btnSquareRoi";
-            this.btnSquareRoi.Size = new System.Drawing.Size(329, 60);
-            this.btnSquareRoi.TabIndex = 0;
-            this.btnSquareRoi.Text = "Single Manually-Placed ROI";
-            this.btnSquareRoi.UseVisualStyleBackColor = true;
-            this.btnSquareRoi.Click += new System.EventHandler(this.btnSquareRoi_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(26, 33);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(478, 21);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "This application measures G/R in multi-frame 2-channel TIF images.";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(26, 78);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(308, 21);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "What type of ROI do you want to measure?";
-            // 
-            // btnDendriteRois
-            // 
-            this.btnDendriteRois.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btnDendriteRois.Location = new System.Drawing.Point(401, 126);
-            this.btnDendriteRois.Name = "btnDendriteRois";
-            this.btnDendriteRois.Size = new System.Drawing.Size(343, 60);
-            this.btnDendriteRois.TabIndex = 3;
-            this.btnDendriteRois.Text = "Multiple ROIs Along a Dendrite";
-            this.btnDendriteRois.UseVisualStyleBackColor = true;
-            this.btnDendriteRois.Click += new System.EventHandler(this.btnDendriteRois_Click);
-            // 
-            // MenuForm
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 222);
-            this.Controls.Add(this.btnDendriteRois);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btnSquareRoi);
-            this.Name = "MenuForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Image Ratio Tool";
-            this.ResumeLayout(false);
-            this.PerformLayout();
-
+        btnSquareRoi = new Button();
+        label1 = new Label();
+        btnDendriteRois = new Button();
+        btnXmlInfo = new Button();
+        SuspendLayout();
+        // 
+        // btnSquareRoi
+        // 
+        btnSquareRoi.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+        btnSquareRoi.Location = new Point(37, 110);
+        btnSquareRoi.Margin = new Padding(4, 5, 4, 5);
+        btnSquareRoi.Name = "btnSquareRoi";
+        btnSquareRoi.Size = new Size(365, 66);
+        btnSquareRoi.TabIndex = 0;
+        btnSquareRoi.Text = "Single Manually-Placed ROI";
+        btnSquareRoi.UseVisualStyleBackColor = true;
+        btnSquareRoi.Click += btnSquareRoi_Click;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+        label1.Location = new Point(37, 41);
+        label1.Margin = new Padding(4, 0, 4, 0);
+        label1.Name = "label1";
+        label1.Size = new Size(736, 32);
+        label1.TabIndex = 1;
+        label1.Text = "This application measures G/R in multi-frame 2-channel TIF images.";
+        // 
+        // btnDendriteRois
+        // 
+        btnDendriteRois.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+        btnDendriteRois.Location = new Point(37, 202);
+        btnDendriteRois.Margin = new Padding(4, 5, 4, 5);
+        btnDendriteRois.Name = "btnDendriteRois";
+        btnDendriteRois.Size = new Size(365, 66);
+        btnDendriteRois.TabIndex = 3;
+        btnDendriteRois.Text = "Multiple ROIs Along a Dendrite";
+        btnDendriteRois.UseVisualStyleBackColor = true;
+        btnDendriteRois.Click += btnDendriteRois_Click;
+        // 
+        // btnXmlInfo
+        // 
+        btnXmlInfo.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
+        btnXmlInfo.Location = new Point(37, 292);
+        btnXmlInfo.Margin = new Padding(4, 5, 4, 5);
+        btnXmlInfo.Name = "btnXmlInfo";
+        btnXmlInfo.Size = new Size(365, 66);
+        btnXmlInfo.TabIndex = 4;
+        btnXmlInfo.Text = "PrairieView XML Information";
+        btnXmlInfo.UseVisualStyleBackColor = true;
+        btnXmlInfo.Click += btnXmlInfo_Click;
+        // 
+        // MenuForm
+        // 
+        AutoScaleDimensions = new SizeF(10F, 25F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(828, 390);
+        Controls.Add(btnXmlInfo);
+        Controls.Add(btnDendriteRois);
+        Controls.Add(label1);
+        Controls.Add(btnSquareRoi);
+        Margin = new Padding(4, 5, 4, 5);
+        Name = "MenuForm";
+        StartPosition = FormStartPosition.CenterScreen;
+        Text = "Image Ratio Tool";
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
 
     private Button btnSquareRoi;
     private Label label1;
-    private Label label2;
     private Button btnDendriteRois;
+    private Button btnXmlInfo;
 }
--- src/ImageRatioTool/Forms/MenuForm.cs
+++ src/ImageRatioTool/Forms/MenuForm.cs
@@ -10,4 +10,6 @@ public partial class MenuForm : Form
     private void btnSquareRoi_Click(object sender, EventArgs e) => new SquareRoiForm().ShowDialog();
 
     private void btnDendriteRois_Click(object sender, EventArgs e) => new DendriteRoiForm().ShowDialog();
+
+    private void btnXmlInfo_Click(object sender, EventArgs e) => new XmlInspectorForm().ShowDialog();
 }
