e7ca17eb187eb73b5109a0e89ded1437fb193f11
Scott W Harden <swharden@gmail.com>
2024-03-06 15:29:09 -0500

    bump minor version

--- src/ScanAGator/Version.cs
+++ src/ScanAGator/Version.cs
@@ -4,7 +4,7 @@ public static class Version
 {
     public static int Major = 4; // bump for changes that break the analysis data or save file format
 
-    public static int Minor = 5; // bump for the addition of new features
+    public static int Minor = 6; // bump for the addition of new features
 
     public static string VersionString => $"Scan-A-Gator v{Major}.{Minor}";
 }
